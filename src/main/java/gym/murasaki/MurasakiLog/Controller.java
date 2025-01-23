47c47ec09b8be55fb0336425fffa8318052676d1
Luis <luisgmgomes@gmail.com>
2025-01-23 01:13:25 -0300

    feat: create controller

---
+++ src/main/java/gym/murasaki/MurasakiLog/Controller.java
@@ -0,0 +1,17 @@
+package gym.murasaki.MurasakiLog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa e a minha primeira messagem nessa rota";
+    }
+
+
+}
