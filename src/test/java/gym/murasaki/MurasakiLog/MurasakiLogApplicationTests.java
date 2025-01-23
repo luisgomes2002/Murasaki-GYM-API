6df2330da5b8d7b796380c9e2787dfd7f9a0423e
Luis <luisgmgomes@gmail.com>
2025-01-23 00:34:05 -0300

    first commit

---
+++ src/test/java/gym/murasaki/MurasakiLog/MurasakiLogApplicationTests.java
@@ -0,0 +1,13 @@
+package gym.murasaki.MurasakiLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurasakiLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
