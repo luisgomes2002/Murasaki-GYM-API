cb2e4f54d340579328f67eb5db61ba05e5b54ed5
Luis <luisgmgomes@gmail.com>
2025-01-30 19:22:23 -0300

    fix: coloca a coluna de email como unique

--- src/main/java/gym/murasaki/MurasakiLog/Users/UsersModel.java
+++ src/main/java/gym/murasaki/MurasakiLog/Users/UsersModel.java
@@ -14,7 +14,10 @@ public class UsersModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
     private UsersEnum type;
 }
